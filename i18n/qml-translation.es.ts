d986c59107db85f20376a6fd3b1caa6401900553
Hauke Schade <cnlpete@cnlpete.de>
2013-01-09 17:11:25 +0100

    added spanish as third language, closes #4

---
+++ i18n/qml-translation.es.ts
@@ -0,0 +1,183 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="es_ES">
+<context>
+    <name>About</name>
+    <message>
+        <location filename="../qml/ttrss/About.qml" line="105"/>
+        <source>Version</source>
+        <translation>versión</translation>
+    </message>
+    <message>
+        <location filename="../qml/ttrss/About.qml" line="106"/>
+        <source>Copyright</source>
+        <translation>copyright</translation>
+    </message>
+    <message>
+        <location filename="../qml/ttrss/About.qml" line="113"/>
+        <source>Homepage</source>
+        <translation>página web</translation>
+    </message>
+</context>
+<context>
+    <name>AboutItem</name>
+    <message>
+        <location filename="../qml/ttrss/AboutItem.qml" line="16"/>
+        <source>About</source>
+        <translation>Sobre</translation>
+    </message>
+</context>
+<context>
+    <name>Categories</name>
+    <message>
+        <location filename="../qml/ttrss/Categories.qml" line="145"/>
+        <source>All Categories</source>
+        <translation>Todas las categorías</translation>
+    </message>
+    <message>
+        <location filename="../qml/ttrss/Categories.qml" line="154"/>
+        <source>No categories to display</source>
+        <translation>No hay categorías para mostrar</translation>
+    </message>
+    <message>
+        <location filename="../qml/ttrss/Categories.qml" line="154"/>
+        <source>No categories have unread items</source>
+        <translation>No hay categorías para mostrar</translation>
+    </message>
+    <message>
+        <location filename="../qml/ttrss/Categories.qml" line="185"/>
+        <source>Tiny Tiny RSS Reader</source>
+        <translation>Tiny Tiny RSS Reader</translation>
+    </message>
+    <message>
+        <location filename="../qml/ttrss/Categories.qml" line="212"/>
+        <source>Toggle Unread Only</source>
+        <translation>Alternar sin leer sólo</translation>
+    </message>
+</context>
+<context>
+    <name>ErrorDialog</name>
+    <message>
+        <location filename="../qml/ttrss/ErrorDialog.qml" line="44"/>
+        <source>OK</source>
+        <translation>Aceptar</translation>
+    </message>
+</context>
+<context>
+    <name>FeedItem</name>
+    <message>
+        <location filename="../qml/ttrss/FeedItem.qml" line="185"/>
+        <source>Open in Web Browser</source>
+        <translation>Abrir en navegador web</translation>
+    </message>
+    <message>
+        <location filename="../qml/ttrss/FeedItem.qml" line="192"/>
+        <source>Mark read</source>
+        <translation>marcar como leído</translation>
+    </message>
+    <message>
+        <location filename="../qml/ttrss/FeedItem.qml" line="192"/>
+        <source>Mark Unread</source>
+        <translation>marcar como no leído</translation>
+    </message>
+</context>
+<context>
+    <name>FeedItems</name>
+    <message>
+        <location filename="../qml/ttrss/FeedItems.qml" line="160"/>
+        <source>No items in feed</source>
+        <translation>No hay artículos para mostrar</translation>
+    </message>
+    <message>
+        <location filename="../qml/ttrss/FeedItems.qml" line="160"/>
+        <source>No unread items in feed</source>
+        <translation>No hay artículos para mostrar</translation>
+    </message>
+    <message>
+        <location filename="../qml/ttrss/FeedItems.qml" line="228"/>
+        <source>Toggle Unread Only</source>
+        <translation>Alternar sin leer sólo</translation>
+    </message>
+    <message>
+        <location filename="../qml/ttrss/FeedItems.qml" line="252"/>
+        <source>Unstar</source>
+        <translation>desmarcar</translation>
+    </message>
+    <message>
+        <location filename="../qml/ttrss/FeedItems.qml" line="252"/>
+        <source>Star</source>
+        <translation>marcar</translation>
+    </message>
+    <message>
+        <location filename="../qml/ttrss/FeedItems.qml" line="261"/>
+        <source>Unpublish</source>
+        <translation>despublicar</translation>
+    </message>
+    <message>
+        <location filename="../qml/ttrss/FeedItems.qml" line="261"/>
+        <source>Publish</source>
+        <translation>publicar</translation>
+    </message>
+    <message>
+        <location filename="../qml/ttrss/FeedItems.qml" line="270"/>
+        <source>Mark read</source>
+        <translation>marcar como leído</translation>
+    </message>
+    <message>
+        <location filename="../qml/ttrss/FeedItems.qml" line="270"/>
+        <source>Mark Unread</source>
+        <translation>desmarcar como leído</translation>
+    </message>
+    <message>
+        <location filename="../qml/ttrss/FeedItems.qml" line="279"/>
+        <source>Open in Web Browser</source>
+        <translation>Abrir en navegador web</translation>
+    </message>
+</context>
+<context>
+    <name>Feeds</name>
+    <message>
+        <location filename="../qml/ttrss/Feeds.qml" line="120"/>
+        <source>Unread: </source>
+        <translation>no leído:</translation>
+    </message>
+    <message>
+        <location filename="../qml/ttrss/Feeds.qml" line="127"/>
+        <source>No feeds in category</source>
+        <translation>No hay Feeds para mostrar</translation>
+    </message>
+    <message>
+        <location filename="../qml/ttrss/Feeds.qml" line="127"/>
+        <source>Category has no unread items</source>
+        <translation>No hay Feeds para mostrar</translation>
+    </message>
+    <message>
+        <location filename="../qml/ttrss/Feeds.qml" line="200"/>
+        <source>Toggle Unread Only</source>
+        <translation>Alternar sin leer sólo</translation>
+    </message>
+</context>
+<context>
+    <name>MainPage</name>
+    <message>
+        <location filename="../qml/ttrss/MainPage.qml" line="103"/>
+        <source>Server:</source>
+        <translation>Servidor:</translation>
+    </message>
+    <message>
+        <location filename="../qml/ttrss/MainPage.qml" line="113"/>
+        <source>Username:</source>
+        <translation>Nombre de usuario:</translation>
+    </message>
+    <message>
+        <location filename="../qml/ttrss/MainPage.qml" line="123"/>
+        <source>Password:</source>
+        <translation>Contraseña:</translation>
+    </message>
+    <message>
+        <location filename="../qml/ttrss/MainPage.qml" line="148"/>
+        <source>Login</source>
+        <translation>Login</translation>
+    </message>
+</context>
+</TS>
