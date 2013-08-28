c6d5fb5e3b59901980308afa9adcfbf44d0928fe
Hauke Schade <cnlpete@cnlpete.de>
2013-08-28 17:56:50 +0200

    removed two debug outputs

--- mynetworkmanager.hh
+++ mynetworkmanager.hh
@@ -49,14 +49,14 @@ private:
     void incNumRequests() {
         QMutexLocker locker(&_mutex);
         _numRequests++;
-        qDebug() << "numReq is now " << _numRequests;
+        //qDebug() << "numReq is now " << _numRequests;
         if (_numRequests == 1)
             loadingChanged();
     }
     void decNumRequests() {
         QMutexLocker locker(&_mutex);
         _numRequests--;
-        qDebug() << "numReq is now " << _numRequests;
+        //qDebug() << "numReq is now " << _numRequests;
         // BUGFIX there are still scenarios where there is an error signal and a finished signal so this gets called twice :(
         // therefor try to limit scenarios where this would be decremented one too many
         if (_numRequests < 0)
