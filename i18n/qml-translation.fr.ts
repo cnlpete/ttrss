5a64a8dd8b4de38b46f0c0e3592b51ac94a0e16b
Hauke Schade <cnlpete@cnlpete.de>
2014-10-07 21:26:57 +0200

    updated french translation, regards #100

--- i18n/qml-translation.fr.ts
+++ i18n/qml-translation.fr.ts
@@ -63,7 +63,7 @@
     <message>
         <location filename="../qml/ttrss/sailfish/pages/AboutPage.qml" line="44"/>
         <source>Full License</source>
-        <translation type="unfinished"/>
+        <translation>Licence complète</translation>
     </message>
     <message>
         <location filename="../qml/ttrss/sailfish/pages/AboutPage.qml" line="99"/>
@@ -73,12 +73,12 @@
     <message>
         <location filename="../qml/ttrss/sailfish/pages/AboutPage.qml" line="118"/>
         <source>Contributors</source>
-        <translation type="unfinished"/>
+        <translation>Contributeurs</translation>
     </message>
     <message>
         <location filename="../qml/ttrss/sailfish/pages/AboutPage.qml" line="134"/>
         <source>Feature Requests &amp; Bugs</source>
-        <translation type="unfinished"/>
+        <translation>Demandes de fonctionnalités &amp; bugs</translation>
     </message>
     <message>
         <location filename="../qml/ttrss/sailfish/pages/AboutPage.qml" line="141"/>
@@ -88,12 +88,12 @@
     <message>
         <location filename="../qml/ttrss/sailfish/pages/AboutPage.qml" line="160"/>
         <source>Legal Notice</source>
-        <translation type="unfinished"/>
+        <translation>Avis juridique</translation>
     </message>
     <message>
         <location filename="../qml/ttrss/sailfish/pages/AboutPage.qml" line="176"/>
         <source>The source code is available at %1.</source>
-        <translation type="unfinished"/>
+        <translation>Le code source est disponible a %1.</translation>
     </message>
 </context>
 <context>
@@ -239,12 +239,12 @@
     <message>
         <location filename="../qml/ttrss/sailfish/pages/FeedDelegate.qml" line="114"/>
         <source>Marking all read</source>
-        <translation type="unfinished"/>
+        <translation>Marquer tout comme lu</translation>
     </message>
     <message>
         <location filename="../qml/ttrss/sailfish/pages/FeedDelegate.qml" line="122"/>
         <source>Unsubcribing</source>
-        <translation type="unfinished"/>
+        <translation>Désabonnement</translation>
     </message>
 </context>
 <context>
@@ -397,7 +397,7 @@
     <message>
         <location filename="../qml/ttrss/sailfish/pages/FeedItems.qml" line="143"/>
         <source>Marking all read</source>
-        <translation type="unfinished"/>
+        <translation>Marquer tout comme lu</translation>
     </message>
 </context>
 <context>
@@ -701,7 +701,7 @@
     <message>
         <location filename="../qml/ttrss/sailfish/pages/Settings.qml" line="115"/>
         <source>Display Labels in Item List</source>
-        <translation type="unfinished"/>
+        <translation>Aff. étiquettes dans la liste d&apos;articles</translation>
     </message>
     <message>
         <location filename="../qml/ttrss/sailfish/pages/Settings.qml" line="161"/>
